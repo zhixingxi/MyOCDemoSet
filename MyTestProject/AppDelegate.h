233b9e611d0367480d3c23fd784f64e5f03a61a7
GT-iOS <276675358@qq.com>
2017-12-04 11:34:11 +0800

    Initial Commit

---
+++ MyTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestProject
+//
+//  Created by GT-iOS on 2017/12/4.
+//  Copyright © 2017年 GT-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestProject
+//
+//  Created by GT-iOS on 2017/12/4.
+//  Copyright © 2017年 GT-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
